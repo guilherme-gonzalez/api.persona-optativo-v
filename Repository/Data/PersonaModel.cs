f929b1db4c5fad7f3f01497a71ceb8e52c8e5546
Guilherme Gonzalez <guilhermegonzlez@gmail.com>
2024-04-02 20:56:55 -0400

    add personas crud

---
+++ Repository/Data/PersonaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data
+{
+    public class PersonaModel
+    {
+        public int Id { get; set; } // This property corresponds to the 'id' field in your database table
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+    }
+}
---
+++ Repository/Data/PersonaRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+
+namespace Repository.Data
+{
+    public class PersonaRepository : IPersona
+    {
+        private readonly IDbConnection _conexionDB;
+
+        public PersonaRepository(string connectionString)
+        {
+            _conexionDB = new DbConection(connectionString).dbConnection();
+        }
+
+        public bool add(PersonaModel persona)
+        {
+            try
+            {
+                var query = "INSERT INTO Persona (nombre, apellido, cedula) VALUES (@Nombre, @Apellido, @Cedula)";
+                var affectedRows = _conexionDB.Execute(query, persona);
+                return affectedRows > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occurred while adding persona.", ex);
+            }
+        }
+
+        public PersonaModel get(int id)
+        {
+            try
+            {
+                var query = "SELECT * FROM Persona WHERE id = @Id";
+                return _conexionDB.QueryFirstOrDefault<PersonaModel>(query, new { Id = id });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occurred while retrieving persona.", ex);
+            }
+        }
+
+        public IEnumerable<PersonaModel> list()
+        {
+            try
+            {
+                var query = "SELECT * FROM Persona";
+                return _conexionDB.Query<PersonaModel>(query);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occurred while listing personas.", ex);
+            }
+        }
+
+        public bool remove(PersonaModel persona)
+        {
+            try
+            {
+                var query = "DELETE FROM Persona WHERE id = @Id";
+                var affectedRows = _conexionDB.Execute(query, new { Id = persona.Id });
+                return affectedRows > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occurred while removing persona.", ex);
+            }
+        }
+
+        public bool update(PersonaModel persona)
+        {
+            try
+            {
+                var query = "UPDATE Persona SET nombre = @Nombre, apellido = @Apellido, cedula = @Cedula WHERE id = @Id";
+                var affectedRows = _conexionDB.Execute(query, persona);
+                return affectedRows > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occurred while updating persona.", ex);
+            }
+        }
+    }
+}
